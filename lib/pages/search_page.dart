c4508383a08b765c204416fb9710b591a1f4af0f
AliShakoori89 <53476225+AliShakoori89@users.noreply.github.com>
2022-12-21 15:18:16 +0330

    edit search page

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:income_and_expenses/utils/app_colors.dart';
 import 'package:income_and_expenses/utils/app_const.dart';
 import 'package:income_and_expenses/utils/arrow_back_icon.dart';
@@ -40,13 +41,25 @@ class SearchPage extends StatelessWidget {
                     return Container(
                       margin: EdgeInsets.all(Dimensions.width10),
                       decoration: BoxDecoration(
-                          border: Border.all(color: AppColors.appBarTitleColor)
+                          border: Border.all(color: AppColors.iconUnSelectedBackGroundMainColor)
                       ),
                       child: Container(
                           margin: EdgeInsets.only(
                               left: Dimensions.width10,
                               right: Dimensions.width10),
-                          child: Text("${AppConst.iconsName[index]}")),
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: Dimensions.width20/1.5,
+                                height: Dimensions.width20/1.5,
+                                child: SvgPicture.asset(
+                                    "assets/logos/${AppConst.iconsImage[index]}",
+                                color: AppColors.labelColor,),
+                              ),
+                              SizedBox(width: Dimensions.width10 / 2,),
+                              Text("${AppConst.iconsName[index]}"),
+                            ],
+                          )),
                     );
                   }),
                 ),
